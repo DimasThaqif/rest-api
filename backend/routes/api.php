1400ae3ac4d0f84769bf73ace2e9df436a604291
dimasthaqif <dimasthaqif10@gmail.com>
2026-02-12 15:45:41 +0700

    fix step 3

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,13 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    Route::apiResource('projects', ProjectController::class);
     Route::get('/me', function (Request $request) {
         return $request->user();
     });
